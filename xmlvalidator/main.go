548eab0e368c17f7ab574b952be8a3b8eb7b6e2c
Jared Johnson <jared@jaredjohnson.dev>
2021-12-25 10:20:09 -0500

    Allow xmlvalidator server to return null strings

--- xmlvalidator/main.go
+++ xmlvalidator/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -21,11 +22,40 @@ const (
 	badRequest = "BAD_REQUEST"
 	validationError = "VALIDATION_ERROR"
 )
+
 var xsdHandler *xsdvalidate.XsdHandler
 
-type ValidateResponse struct {
+type nullableString struct {
+	sql.NullString
+}
+
+type validateResponse struct {
 	Code string `json:"code"`
-	Error string `json:"error,omitempty"`
+	Error nullableString `json:"error"`
+}
+
+func (s nullableString) MarshalJSON() ([]byte, error) {
+	if !s.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(s.String)
+}
+
+func newValidateResponse(code string, err error) validateResponse {
+	var errorStr nullableString
+	if err == nil {
+		errorStr = nullableString{
+			NullString: sql.NullString{Valid: false},
+		}
+	} else {
+		errorStr = nullableString{
+			NullString: sql.NullString{String: err.Error(), Valid: true},
+		}
+	}
+	return validateResponse{
+		Code: code,
+		Error: errorStr,
+	}
 }
 
 func validate(w http.ResponseWriter, req *http.Request) {
@@ -39,22 +69,23 @@ func validate(w http.ResponseWriter, req *http.Request) {
 	encoder := json.NewEncoder(w)
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		encoder.Encode(ValidateResponse{badRequest, err.Error()})
+		encoder.Encode(newValidateResponse(badRequest, err))
 		return
 	}
 
 	err = xsdHandler.ValidateMem(body, xsdvalidate.ParsErrVerbose)
 	if err != nil {
-		encoder.Encode(ValidateResponse{validationError, err.Error()})
+		encoder.Encode(newValidateResponse(validationError, err))
 		return
 	}
 
-	encoder.Encode(ValidateResponse{ok, ""})
+	encoder.Encode(newValidateResponse(ok, nil))
 }
 
 func main() {
 	xsdvalidate.Init()
 	defer xsdvalidate.Cleanup()
+
 	var err error
 	xsdHandler, err = xsdvalidate.NewXsdHandlerUrl(schemaUrl, xsdvalidate.ParsErrDefault)
 	defer xsdHandler.Free()
@@ -66,7 +97,9 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
+
 	http.HandleFunc("/validate", validate)
+
 	fmt.Println(fmt.Sprintf("running on port %s", port))
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
