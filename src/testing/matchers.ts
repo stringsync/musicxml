5efa71934d55119123fa03dc8ff2fc8168fa5d2c
Jared Johnson <jared@jaredjohnson.dev>
2021-12-26 10:37:27 -0500

    Improve readability of ValidationResponse

--- src/testing/matchers.ts
+++ src/testing/matchers.ts
@@ -3,18 +3,9 @@ import fetch from 'node-fetch';
 const VALIDATE_URL = 'http://xmlvalidator:8080/validate';
 
 type ValidationResponse =
-  | {
-      code: 'OK';
-      error: null;
-    }
-  | {
-      code: 'BAD_REQUEST';
-      error: string;
-    }
-  | {
-      code: 'VALIDATION_ERROR';
-      error: string;
-    };
+  | { code: 'OK'; error: null }
+  | { code: 'BAD_REQUEST'; error: string }
+  | { code: 'VALIDATION_ERROR'; error: string };
 
 const validate = async (xml: string): Promise<ValidationResponse> => {
   const res = await fetch(VALIDATE_URL, {
