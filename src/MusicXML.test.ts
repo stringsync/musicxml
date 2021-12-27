80f90d196d1738d77464b3929543d513ec413430
Jared Johnson <jared@jaredjohnson.dev>
2021-12-27 10:38:42 -0500

    Simplify MusicXML test

--- src/MusicXML.test.ts
+++ src/MusicXML.test.ts
@@ -2,7 +2,6 @@ import { MusicXML } from './MusicXML';
 
 describe('MusicXML', () => {
   it('can be created without crashing', () => {
-    const musicXML = new MusicXML();
-    expect(musicXML).toBeInstanceOf(MusicXML);
+    expect(() => new MusicXML()).not.toThrow();
   });
 });
