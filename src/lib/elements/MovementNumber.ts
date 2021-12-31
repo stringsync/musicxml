a8287ad3628cecebff1cf4d9267e8bac43f7db24
Jared Johnson <jared@jaredjohnson.dev>
2021-12-31 11:42:08 -0500

    Add missing backticks

--- src/lib/elements/MovementNumber.ts
+++ src/lib/elements/MovementNumber.ts
@@ -3,9 +3,9 @@ import { element } from '../xml';
 export type MovementNumber = ReturnType<typeof MovementNumber>;
 
 /**
- * Parent elements: <score-partwise>, <score-timewise>
+ * Parent elements: `<score-partwise>`, `<score-timewise>`
  *
- * The <movement-number> element specifies the number of a movement.
+ * The `<movement-number>` element specifies the number of a movement.
  *
  * {@link https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/movement-number/}
  */
--- src/lib/elements/MovementTitle.ts
+++ src/lib/elements/MovementTitle.ts
@@ -3,9 +3,9 @@ import { element } from '../xml';
 export type MovementTitle = ReturnType<typeof MovementTitle>;
 
 /**
- * Parent elements: <score-partwise>, <score-timewise>
+ * Parent elements: `<score-partwise>`, `<score-timewise>`
  *
- * The <movement-title> element specifies the title of a movement, not including its number.
+ * The `<movement-title>` element specifies the title of a movement, not including its number.
  *
  * {@link https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/movement-title/}
  */
