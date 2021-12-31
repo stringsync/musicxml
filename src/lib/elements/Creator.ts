51b3e3b1aeae1133d14c03fa365befde6a49a9c5
Jared Johnson <jared@jaredjohnson.dev>
2021-12-31 13:22:48 -0500

    Add Creator element

---
+++ src/lib/elements/Creator.ts
@@ -0,0 +1,21 @@
+import { t, xml } from '../xml';
+
+/**
+ * Parent elements: `<score-part>`, `<score-partwise>`, `<score-timewise>`
+ *
+ * The `<identification>` element contains basic metadata about the score. It includes information that may apply at a
+ * score-wide, movement-wide, or part-wide level. The `<creator>`, `<rights>`, `<source>`, and `<relation>` elements are
+ * based on Dublin Core.
+ *
+ * {@link https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/identification/}
+ */
+export const Creator = xml.element(
+  'creator',
+  {
+    attributes: {
+      type: t.optional(t.string()),
+    },
+    content: [t.string()],
+  },
+  {}
+);
