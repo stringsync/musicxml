9e632232639a4163f240be16b29c9f232d25ee42
Jared Johnson <jared@jaredjohnson.dev>
2021-12-31 13:26:46 -0500

    Add Encoding element

---
+++ src/lib/elements/Encoding.ts
@@ -0,0 +1,13 @@
+import { xml } from '../xml';
+
+/**
+ * Parent element: `<identification>`
+ *
+ * The `<encoding>` element contains information about who did the digital encoding, when, with what software, and in
+ * what aspects.
+ *
+ * {@link https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/encoding/}
+ */
+export type Encoding = ReturnType<typeof Encoding>;
+
+export const Encoding = xml.element('encoding', { attributes: {}, content: [] }, {});
