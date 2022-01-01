75a6f7ba6577938dedb017263626e039983bbc2b
Jared Johnson <jared@jaredjohnson.dev>
2022-01-01 01:09:25 -0500

    Add SystemMargins element

---
+++ src/lib/elements/SystemMargins.test.ts
@@ -0,0 +1,7 @@
+import { SystemMargins } from './SystemMargins';
+
+describe('SystemMargins', () => {
+  it('runs without crashing', () => {
+    expect(SystemMargins).not.toThrow();
+  });
+});
---
+++ src/lib/elements/SystemMargins.ts
@@ -0,0 +1,18 @@
+import { t, xml } from '../xml';
+import { LeftMargin } from './LeftMargin';
+import { RightMargin } from './RightMargin';
+
+/**
+ * Parent element: `<system-layout>`
+ *
+ * System margins are relative to the page margins. Positive values indent and negative values reduce the margin size.
+ *
+ * {@link https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/system-margins/}
+ */
+export type SystemMargins = ReturnType<typeof SystemMargins>;
+
+export const SystemMargins = xml.element(
+  'system-margins',
+  { attributes: {}, content: [t.required(LeftMargin), t.required(RightMargin)] as const },
+  {}
+);
