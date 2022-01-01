6dd9d3f489444c54379983ea44e3addfcbe0c3f4
Jared Johnson <jared@jaredjohnson.dev>
2022-01-01 01:07:56 -0500

    Fix SystemLayout annotation

--- src/lib/elements/SystemLayout.ts
+++ src/lib/elements/SystemLayout.ts
@@ -1,13 +1,27 @@
 import { xml } from '../xml';
 
 /**
- * Parent elements: `<score-partwise>`, `<score-timewise>`
+ * Parent elements: `<defaults>`, `<print>`
  *
- * The `<defaults>` element specifies score-wide defaults for scaling; whether or not the file is a concert score;
- * layout; and default values for the music font, word font, lyric font, and lyric language. Except for the
- * `<concert-score>` element, if any defaults are missing, the choice of what to use is determined by the application.
+ * A system is a group of staves that are read and played simultaneously. The `<system-layout>` element includes left
+ * and right margins and the vertical distance from the previous system.
  *
- * {@link https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/defaults/}
+ * Sometimes the sum of measure widths in a system may not equal the system width specified by the layout elements due
+ * to roundoff or other errors. The behavior when reading MusicXML files in these cases is application-dependent. For
+ * instance, applications may find that the system layout data is more reliable than the sum of the measure widths, and
+ * adjust the measure widths accordingly.
+ *
+ * When used in the `<defaults>` element, the `<system-layout>` element defines a default appearance for all systems in
+ * the score. If no `<system-layout>` element is present in the `<defaults>` element, default system layout values are
+ * chosen by the application.
+ *
+ * When used in the `<print>` element, the `<system-layout>` element affects the appearance of the current system only.
+ * All other systems use the default values as determined by the `<defaults>` element. If any child elements are missing
+ * from the `<system-layout>` element in a `<print>` element, the values determined by the `<defaults>` element are used
+ * there as well. This type of `<system-layout>` element need only be read from or written to the first visible part in
+ * the score.
+ *
+ * {@link https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/system-layout/}
  */
 export type SystemLayout = ReturnType<typeof SystemLayout>;
 
