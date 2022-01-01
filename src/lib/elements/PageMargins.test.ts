1841047fa5b20c4257a0b8f564c52b2c1d454e52
Jared Johnson <jared@jaredjohnson.dev>
2022-01-01 00:51:39 -0500

    Add PageMargins element

---
+++ src/lib/elements/PageMargins.test.ts
@@ -0,0 +1,7 @@
+import { PageMargins } from './PageMargins';
+
+describe('PageMargins', () => {
+  it('runs without crashing', () => {
+    expect(PageMargins).not.toThrow();
+  });
+});
---
+++ src/lib/elements/PageMargins.ts
@@ -0,0 +1,20 @@
+import { t, xml } from '../xml';
+
+/**
+ * Parent element: `<page-layout>`
+ *
+ * The `<page-margins>` element specifies page margins in tenths either for both even and odd pages, or via separate odd
+ * and even page number values.
+ *
+ * {@link https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/page-margins/}
+ */
+export type PageMargins = ReturnType<typeof PageMargins>;
+
+export const PageMargins = xml.element(
+  'page-margins',
+  {
+    attributes: { type: t.optional(t.choices('both' as const, 'even' as const, 'odd' as const)) },
+    content: [] as const,
+  },
+  {}
+);
