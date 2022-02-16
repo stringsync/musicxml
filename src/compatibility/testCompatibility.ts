8599455db2e04202b999b992766f393b298cc245
Jared Johnson <jared@jaredjohnson.dev>
2022-02-16 18:38:13 -0500

    Document testCompatibility

--- src/compatibility/testCompatibility.ts
+++ src/compatibility/testCompatibility.ts
@@ -1,6 +1,12 @@
 import * as examples from '../examples';
 import { MusicXML } from '../MusicXML';
 
+/**
+ * Adds a jest test that all of the examples in the suite are valid MusicXML and that this library can losslessly parse
+ * and serialize the document.
+ *
+ * @param suite {Array<examples.Example>} the examples to test
+ */
 export const testCompatibility = (suite: readonly examples.Example[]) => {
   it.each(suite)('preserves the contents of %s', async (example) => {
     const xmlStr = examples.loadExample(example);
