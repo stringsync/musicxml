3f0d668eafb52b72037e36337e0c56416a543eaf
Jared Johnson <jared@jaredjohnson.dev>
2022-02-16 19:27:46 -0500

    Parallelize osmd tests since they were taking a long time

--- src/compatibility/osmd_1.test.ts
+++ src/compatibility/osmd_1.test.ts
@@ -4,5 +4,5 @@ import { testCompatibility } from './testCompatibility';
 describe('osmd', () => {
   // Test that the examples in https://github.com/opensheetmusicdisplay/opensheetmusicdisplay are compatible with this
   // library.
-  testCompatibility(EXAMPLE_SUITES.OSMD);
+  testCompatibility(EXAMPLE_SUITES.OSMD, { numChunks: 4, chunkIndex: 1 });
 });
--- src/compatibility/testCompatibility.ts
+++ src/compatibility/testCompatibility.ts
@@ -1,5 +1,11 @@
 import * as examples from '../examples';
+import { MusicXMLError } from '../lib/errors';
 import { MusicXML } from '../MusicXML';
+
+export type TestCompatibilityOpts = {
+  numChunks: number;
+  chunkIndex: number;
+};
 
 /**
  * Adds a jest test that all of the examples in the suite are valid MusicXML and that this library can losslessly parse
@@ -7,11 +13,34 @@ import { MusicXML } from '../MusicXML';
  *
  * @param suite {Array<examples.Example>} the examples to test
  */
-export const testCompatibility = (suite: readonly examples.Example[]) => {
-  it.each(suite)('preserves the contents of %s', async (example) => {
+export const testCompatibility = (
+  suite: readonly examples.Example[],
+  opts: TestCompatibilityOpts = { numChunks: 1, chunkIndex: 0 }
+) => {
+  if (opts.chunkIndex > opts.numChunks - 1 || opts.chunkIndex < 0 || opts.numChunks <= 0) {
+    throw new MusicXMLError({
+      symptom: 'invalid options',
+      context: { opts },
+      remedy: 'fix the options to have valid values',
+    });
+  }
+
+  const chunkSize = Math.floor(suite.length / opts.numChunks);
+  const subsuite = getChunks([...suite], chunkSize)[opts.chunkIndex];
+
+  it.each(subsuite)('preserves the contents of %s', async (example) => {
     const xmlStr = examples.loadExample(example);
     await expect(xmlStr).toBeValidMusicXML();
     const musicXml = MusicXML.parse(xmlStr);
     expect(musicXml.serialize()).toEqualXML(xmlStr);
   });
+};
+
+const getChunks = <T>(arr: T[], chunkSize: number): T[][] => {
+  const result = new Array<T[]>();
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    result.push(chunk);
+  }
+  return result;
 };
