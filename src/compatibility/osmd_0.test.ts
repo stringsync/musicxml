831d0d2cd5543299eaee25338116e2d1a057ac82
Jared Johnson <jared@jaredjohnson.dev>
2022-02-17 07:57:33 -0500

    Remove janky parallelization

--- src/compatibility/osmd_0.test.ts
+++
@@ -1,8 +0,0 @@
-import { EXAMPLE_SUITES } from '../examples';
-import { testCompatibility } from './testCompatibility';
-
-describe('osmd', () => {
-  // Test that the examples in https://github.com/opensheetmusicdisplay/opensheetmusicdisplay are compatible with this
-  // library.
-  testCompatibility(EXAMPLE_SUITES.OSMD, { numChunks: 4, chunkIndex: 0 });
-});
