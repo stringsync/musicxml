19fa0e4cbcb712dd38e053c8f179640e52200a51
Jared Johnson <jared@jaredjohnson.dev>
2022-02-26 11:53:37 -0500

    Add parse tests

--- src/xml/parse.test.ts
+++ src/xml/parse.test.ts
@@ -25,6 +25,27 @@ describe('parse', () => {
     });
   });
 
+  it('parses nested elements', () => {
+    const result = parse('<foo><bar></bar></foo>');
+    expect(result).toStrictEqual({
+      declaration: { attributes: { encoding: 'UTF-8', version: '1.0' } },
+      root: {
+        type: 'element',
+        name: 'foo',
+        attributes: {},
+        contents: [{ type: 'element', name: 'bar', attributes: {}, contents: [] }],
+      },
+    });
+  });
+
+  it('parses attributes', () => {
+    const result = parse('<root foo="foo" bar="42" baz="False"></root>');
+    expect(result).toStrictEqual({
+      declaration: { attributes: { encoding: 'UTF-8', version: '1.0' } },
+      root: { type: 'element', name: 'root', attributes: { foo: 'foo', bar: '42', baz: 'False' }, contents: [] },
+    });
+  });
+
   it('throws an error when there is more than one root', () => {
     expect(() => parse('<root1></root1><root2></root2>')).toThrow();
   });
