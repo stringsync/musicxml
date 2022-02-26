d3fb300a0c430ba7c18c99c1e303826cb728c966
Jared Johnson <jared@jaredjohnson.dev>
2022-02-26 11:48:03 -0500

    Add parse tests

--- src/xml/parse.test.ts
+++ src/xml/parse.test.ts
@@ -1,11 +1,31 @@
 import { parse } from './parse';
 
 describe('parse', () => {
-  it('parses a simple document', () => {
+  it('parses a document with a declaration and root', () => {
     const result = parse('<?xml version="1.0" encoding="UTF-8" ?><root></root>');
     expect(result).toStrictEqual({
       declaration: { attributes: { encoding: 'UTF-8', version: '1.0' } },
       root: { type: 'element', name: 'root', attributes: {}, contents: [] },
     });
+  });
+
+  it('parses a document with just a root', () => {
+    const result = parse('<root></root>');
+    expect(result).toStrictEqual({
+      declaration: { attributes: { encoding: 'UTF-8', version: '1.0' } },
+      root: { type: 'element', name: 'root', attributes: {}, contents: [] },
+    });
+  });
+
+  it('parses a self closing root', () => {
+    const result = parse('<root/>');
+    expect(result).toStrictEqual({
+      declaration: { attributes: { encoding: 'UTF-8', version: '1.0' } },
+      root: { type: 'element', name: 'root', attributes: {}, contents: [] },
+    });
+  });
+
+  it('throws an error when there is more than one root', () => {
+    expect(() => parse('<root1></root1><root2></root2>')).toThrow();
   });
 });
